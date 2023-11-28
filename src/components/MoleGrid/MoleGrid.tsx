edc88f4495a8bdd61f62fa851291f35821be8832
QuickerMaths <mateusz.szalowicz@gmail.com>
2023-11-28 11:20:05 +0100

    implemented random mole interval

--- src/components/MoleGrid/MoleGrid.tsx
+++ src/components/MoleGrid/MoleGrid.tsx
@@ -1,37 +1,39 @@
+import { useEffect, useState } from "react";
 import hole from "../../assets/hole.png";
 import mole from "../../assets/mole.png";
 import styles from "./MoleGrid.module.css";
 
 const MoleGrid = () => {
+  const [holesMoles, setHolesMoles] = useState<boolean[]>(
+    new Array(9).fill(false)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * holesMoles.length);
+      setHolesMoles(
+        holesMoles.map((holeOrMole, index) =>
+          index === randomIndex ? (holeOrMole = true) : holeOrMole
+        )
+      );
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <ul className={styles.container}>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
-      <li className={styles.item}>
-        <img src={hole} alt="Hole" className={styles.img} />
-      </li>
+      {holesMoles.map((holeOrMole, index) => (
+        <li className={styles.item} key={index}>
+          <img
+            src={holeOrMole ? mole : hole}
+            alt={holeOrMole ? "Mole" : "Hole"}
+            className={styles.img}
+          />
+        </li>
+      ))}
     </ul>
   );
 };
--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,10 +1,9 @@
-import styles from "./Game.module.css";
 import Score from "../../components/Score/Score";
 import MoleGrid from "../../components/MoleGrid/MoleGrid";
 
 const Game = () => {
   return (
-    <main className={styles.main}>
+    <main>
       <Score />
       <MoleGrid />
     </main>
